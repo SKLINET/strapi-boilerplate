62694e041f86ecdfc6129da3d8ead69b2e50f21d
JakubVavra <69152298+JakubVavra@users.noreply.github.com>
2022-09-19 20:30:42 +0200

    Fix SEO and add progressbar

--- frontend/src/components/base/Head/Head.tsx
+++ frontend/src/components/base/Head/Head.tsx
@@ -4,66 +4,73 @@ import { AppContextProps } from '@symbio/headless';
 import config from '../../../../sklinet.config.json';
 import { WebSettingsProps } from '../../../types/webSettings';
 import { PageProps } from '../../../types/page';
-import { useRouter } from 'next/router';
 import { getImageUrl } from '../../../utils/getImageUrl';
-import { MetaItem } from '../../../types/metaItem';
+import { useRouter } from 'next/router';
 
 export interface HeadProps {
-    item?: MetaItem;
+    item?: AppContextProps<PageProps, WebSettingsProps>['item'];
     page?: AppContextProps<PageProps, WebSettingsProps>['page'];
     site: WebSettingsProps;
 }
 
-export const Head = ({ item, page, site }: HeadProps): ReactElement => {
-    const siteObj = site?.data?.attributes?.seo;
-    const data = {
-        title: item?.seo?.title || item?.title || page?.seo?.title || page?.title || siteObj?.title || '',
-        metaTitle: item?.seo?.metaTitle || page?.seo?.metaTitle || siteObj?.metaTitle || '',
-        metaDescription: item?.seo?.metaDescription || page?.seo?.metaDescription || siteObj?.metaDescription || '',
-        social: item?.seo?.metaSocial || page?.seo?.metaSocial || siteObj?.metaSocial || [],
-        viewPort: item?.seo?.metaViewport || page?.seo?.metaViewport || siteObj?.metaViewport || '',
-        keyWords: item?.seo?.keywords || page?.seo?.keywords || siteObj?.keywords || '',
-        robots: item?.seo?.metaRobots || page?.seo?.metaRobots || siteObj?.metaRobots || '',
-        canonical: item?.seo?.canonicalURL || page?.seo?.canonicalURL || siteObj?.canonicalURL || '',
+type MetaSocialItem = {
+    socialNetwork: string;
+    title: string;
+    description: string;
+    image: {
+        data: {
+            attributes: {
+                url: string;
+                width: number;
+                height: number;
+                alternativeText: string;
+                name: string;
+                caption: string;
+            };
+        };
     };
-    const preventIndexing = item?.seo ? item?.seo?.preventIndexing : page?.seo ? page.seo.preventIndexing : false;
-    const meta = item?.seo?.meta || page?.seo?.meta || siteObj?.meta;
+};
+
+type Meta = { name: string | null; content: string | null } | null;
+
+export const Head = ({ page, site }: HeadProps): ReactElement => {
+    const data = {
+        title: page?.meta?.title || page?.meta?.metaTitle || page?.title || '',
+        metaTitle: page?.meta?.metaTitle,
+        metaDescription: page?.meta?.metaDescription,
+        metaImage: page?.meta?.metaImage?.data?.attributes?.url,
+        social: page?.meta?.metaSocial,
+        viewPort: page?.meta?.metaViewport,
+        keyWords: page?.meta?.keywords,
+        robots: page?.meta?.metaRobots,
+        canonical: page?.meta?.canonicalURL,
+    };
+    const preventIndexing = page?.meta ? page?.meta?.preventIndexing : false;
+    const meta = page?.meta?.meta || [];
+
     const router = useRouter();
     const currentPageUrl = process.env.BASE_PATH + router?.asPath;
+
     return (
         <NextHead>
-            <meta
-                name="viewport"
-                content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-            />
-
-            {/* APP */}
-            <link rel="manifest" href="/manifest.json" />
-            <meta name="msapplication-TileColor" content="#00aba9" />
-            <meta name="theme-color" content="#61279e" />
-            {data?.title && <meta name="application-name" content={data.title} />}
-            <meta name="apple-mobile-web-app-capable" content="yes" />
-            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-            {data?.title && <meta name="apple-mobile-web-app-title" content={data.title} />}
-            <meta name="format-detection" content="telephone=no" />
-            <meta name="mobile-web-app-capable" content="yes" />
-            <meta name="msapplication-config" content="/browserconfig.xml" />
-            <meta name="author" content="SKLINET s.r.o." />
-            <meta name="owner" content="SKLINET s.r.o." />
-            <meta name="copyright" content="SKLINET s.r.o." />
+            {/* Meta */}
             <title>{data?.title}</title>
             <meta name="title" content={`${data?.metaTitle || data?.title}`} />
             <meta name="description" content={data?.metaDescription || ''} />
+            {data.metaImage && <meta name="image" content={getImageUrl(data.metaImage)} />}
+            <meta name="keywords" content={data?.keyWords || ''} />
             {preventIndexing ? (
                 <meta name="robots" content="noindex, nofollow" />
             ) : (
                 <meta name="robots" content={`${data?.robots || 'index, follow'}`} />
             )}
-            {meta?.map((m, key) => (
-                <meta key={`meta-${key}`} property={m?.name || ''} content={m?.content || ''} />
-            ))}
-            {data?.social?.length > 0 &&
-                data?.social?.map((item, i: number) => (
+            {meta?.map((m: Meta, key: number) => {
+                if (!m || !m.name || !m.content) return null;
+                return <meta key={`meta-${key}`} property={m?.name} content={m?.content} />;
+            })}
+            {Array.isArray(data?.social) &&
+                data.social.length > 0 &&
+                data.social.map((item: MetaSocialItem, i: number) => (
                     <React.Fragment key={`meta-social-tag-${i}`}>
                         <meta
                             property={item?.socialNetwork === 'Facebook' ? 'og:title' : 'twitter:title'}
@@ -90,18 +97,43 @@ export const Head = ({ item, page, site }: HeadProps): ReactElement => {
                     </React.Fragment>
                 ))}
             {data?.canonical && <link rel="canonical" href={data?.canonical || ''} />}
+            <meta name="msapplication-TileColor" content="#FFFFFF" />
+            <meta name="theme-color" content="#FFFFFF" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
+
+            {/* Favicon */}
+            {/* <link rel="shortcut icon" href="/favicon/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" /> */}
+
+            {/* APP */}
+            {/* <link rel="manifest" href="/manifest.json" /> */}
+            {site?.data?.attributes?.globalSeo?.siteName && (
+                <meta name="application-name" content={site.data.attributes.globalSeo.siteName} />
+            )}
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+            {site?.data?.attributes?.globalSeo?.siteName && (
+                <meta name="apple-mobile-web-app-title" content={site.data.attributes.globalSeo.siteName} />
+            )}
+            <meta name="format-detection" content="telephone=no" />
+            <meta name="mobile-web-app-capable" content="yes" />
+            <meta name="msapplication-config" content="/browserconfig.xml" />
 
             {/* GTM */}
-
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            {config.gtm.code && (
+                // eslint-disable-next-line @next/next/next-script-for-ga
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${site?.data?.attributes?.gtmCode || config.gtm.code}');`,
-                }}
-            />
+                    }}
+                />
+            )}
         </NextHead>
     );
 };
