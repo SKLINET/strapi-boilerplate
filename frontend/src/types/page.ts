9d1d6e507b944490490405608a71406e1e109d07
Jakub Bednár <j.bednar1997@gmail.com>
2022-12-19 14:31:07 +0100

    Update CMS packages and patches

--- frontend/src/types/page.ts
+++ frontend/src/types/page.ts
@@ -1,3 +1,3 @@
 import { pageDetailQueryResponse } from '../relay/__generated__/pageDetailQuery.graphql';
 
-export type PageProps = NonNullable<pageDetailQueryResponse['item'] & { id: string }>;
+export type PageProps = NonNullable<pageDetailQueryResponse['item'] & { url: string; id: string }>;
