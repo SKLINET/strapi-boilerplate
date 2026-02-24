6845eb2a1769455e62487fa0aff44d954c063698
JakubVavra <69152298+JakubVavra@users.noreply.github.com>
2026-02-24 14:01:25 +0100

    Fix build

--- frontend/relay.config.js
+++ frontend/relay.config.js
@@ -3,5 +3,4 @@ module.exports = {
     schema: './src/schema/schema.graphql',
     exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
     language: 'typescript',
-    artifactDirectory: './src/relay/__generated__',
 };
